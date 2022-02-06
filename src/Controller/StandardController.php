e12e94fd93033087c7f3b9a318a6edf1b26e606c
zearkiatos <caprilespe@outlook.com>
2022-02-06 16:23:36 -0300

    Using composer

--- src/Controller/StandardController.php
+++ src/Controller/StandardController.php
@@ -2,24 +2,32 @@
 
 namespace App\Controller;
 
-use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use App\Entity\User;
 use App\Form\UserType;
 
 class StandardController extends AbstractController
 {
+    #[Route('/', name: 'index')]
+    public function index() {
+        return new JsonResponse(['success' => 'Your login was successfully']);
+    }
     #[Route('/register', name: 'register')]
-    public function index(EntityManager $entityManager, Request $request): Response
+    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
         $registerForm = $this->createForm(UserType::class, $user);
         $registerForm->handleRequest($request);
 
         if ($registerForm->isSubmitted() && $registerForm->isValid()) {
+            $hashedPassword = $passwordHasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($hashedPassword);
             $entityManager->persist($user);
             $entityManager->flush();
         }
