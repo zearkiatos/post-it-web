650c70c7408e39db24a1bd429141e4ba9c20a0ae
zearkiatos <caprilespe@outlook.com>
2022-02-10 07:55:40 -0300

    [Working in Progress] How see and comment a post

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -32,6 +32,9 @@ class Post
     #[ORM\JoinColumn(nullable: false)]
     private $category;
 
+    #[ORM\Column(type: 'string', length: 255)]
+    private $title;
+
     public function __construct()
     {
         $this->comments = new ArrayCollection();
@@ -116,6 +119,18 @@ class Post
     public function setCategory(?Category $category): self
     {
         $this->category = $category;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
 
         return $this;
     }
